8b5b3bc5908d21352e5e8530b81c826587c0b25f
Paul G Brown <paulbrown@sequoiacg.com>
2017-12-12 10:51:43 -0600

    Setup the initial project and created an example of immutable.

---
+++ ClassLibrary1/FullNameImmutable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// This is an example of coding an immutable struct
+    /// </summary>
+    public struct FullNameImmutable
+    {
+        public FullNameImmutable(string f, string m, string l)
+        {
+            First = f;
+            Middle = m;
+            Last = l;
+        }
+
+        public string First { get; private set; }
+
+        public string Middle { get; private set; }
+
+        public string Last { get; private set; }
+    }
+}
---
+++ UnitTestProject1/FullNameImmutableShould.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FullNameImmutableShould
+    {
+        [TestMethod]
+        public void Still_have_a_parameterless_constructor()
+        {
+            var fullName = new FullNameImmutable();
+
+            Console.WriteLine("First: " + fullName.First);
+            Console.WriteLine("Middle: " + fullName.Middle);
+            Console.WriteLine("Last: " + fullName.Last);
+        }
+
+        [TestMethod]
+        public void Use_the_parameter_constructor()
+        {
+            var fullName = new FullNameImmutable("John", "Q", "Public");
+
+            Console.WriteLine("First: " + fullName.First);
+            Console.WriteLine("Middle: " + fullName.Middle);
+            Console.WriteLine("Last: " + fullName.Last);
+        }
+
+        /// <summary>
+        /// This will not compile. Uncomment to see that
+        /// first name is inaccessible.
+        /// </summary>
+        //[TestMethod]
+        //public void Not_allow_a_value_to_change()
+        //{
+        //    var fullName = new FullNameImmutable("John", "Q", "Public");
+
+        //    fullName.First = "Fred";
+
+        //    Console.WriteLine("First: " + fullName.First);
+        //    Console.WriteLine("Middle: " + fullName.Middle);
+        //    Console.WriteLine("Last: " + fullName.Last);
+        //}
+    }
+}
